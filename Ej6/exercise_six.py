f15d5384d1408edb806c05aae9b2a75b3b432dda
crisnicogonzalez <cristian3629@gmail.com>
2020-06-29 23:11:09 -0300

    fix

--- Ej6/exercise_six.py
+++ Ej6/exercise_six.py
@@ -6,23 +6,21 @@ probabilties = {2:1.0/36,3:2.0/36,4:3.0/36,5:4.0/36,6:5.0/36,7:6.0/36,8:5.0/36,9
 expected = {}
 
 for key in actual:
-    expected[key] = probabilties[key]*10000
+    expected[key] = probabilties[key]
 
-print("expected: {}".format(str(expected)))
-
-
-frecuencias = actual.values()
-lanzamientos = sum(frecuencias)
-print("lanzamientos "+str(lanzamientos))
+probabilidades_actuales = list(map(lambda key :key*1.0 / 10000, actual.values()))
+print("actual "+str(probabilidades_actuales))
+print("expected "+str(expected.values()))
 nivelDeSignificacion = 0.01
 
-(D2,p_value) = st.chisquare(f_obs = frecuencias, f_exp = expected.values())
+
+
+(D2,p_value) = st.chisquare(f_obs = probabilidades_actuales, f_exp = expected.values())
 
 print("D2_2: {}".format(D2))
 print("P_value: {}".format(p_value))
 
-if p_value < nivelDeSignificacion:
-    print("Se rechaza hipotesis H0")
+if p_value >= nivelDeSignificacion:
+    print("Aceptamos ")
 else:
-    print("No hay evidencia suficiente para rechazar H0")
-
+    print("Rechazamos")
